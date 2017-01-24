c9565f0a33f636e6803fc092396715ff1765ddd2
matetipping <matetipping@gmail.com>
2017-01-24 17:02:22 +1100

    Update current_emblems_10.js

--- current_emblems_10.js
+++ current_emblems_10.js
@@ -1,6 +1,6 @@
 var emblem_data = {
     emblems: [
-        ["Administrator", "rgb(16, 37, 63)", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-750px 0px", "0", "http://b3.ifrm.com/30609/91/0/p3003840/emblemdesigns_tangerine.png", "-450px, 0px", "0"]
+        ["Administrator", "rgb(16, 37, 63)", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-750px 0px", "0", "http://b3.ifrm.com/30609/91/0/p3003840/emblemdesigns_tangerine.png", "-450px 0px", "0"]
     ]
 }
 
