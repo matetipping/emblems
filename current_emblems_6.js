061f15e662c87ec7f6fa1366596a5dadcf6ec98e
matetipping <matetipping@gmail.com>
2017-01-24 16:48:25 +1100

    Update and rename current_emblems_5.js to current_emblems_6.js

--- current_emblems_6.js
+++ current_emblems_6.js
@@ -2,10 +2,10 @@ var emblem_data = {
     emblems: [
         ["Administrator", "rgb(16, 37, 63)"]
     ]
-};
+}
 
 function set_back_col(id, colour) {
-    $(id + ".emblem").css({ background: colour });
+    document.getElementById(id).style.backgroundColor = colour;
 }
 
 // returns the player's name
@@ -13,16 +13,15 @@ function get_this_player() {
     return $("div#top_info strong a").html ();
 }
 
-function set_emblem(id, name, emblems) {
+function set_emblem(id, name, emblems_all) {
     var name_chk = "";
     var i;
-    var len = emblems.length;
+    var len = emblems_all.length;
     for (i = 0; i < len; i++) {
-        name_chk = emblems[i][0];
+        name_chk = emblems_all[i][0];
         if (name_chk === name) {
-            var back_col = emblems[i][1];
-            set_back_col(id, back_col);
-            return;
+            var back_colour = emblems_all[i][1];
+            set_back_col(id, back_colour);
         }
     }
 }
@@ -31,4 +30,3 @@ $(function () {
     var player_name = get_this_player();
     set_emblem("emblem_user", player_name, emblem_data.emblems);
 });
- 
