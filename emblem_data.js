51c49e3d9acbdda9e5bc1a1b83446bfc5207b073
matetipping <matetipping@gmail.com>
2017-02-07 16:56:34 +1100

    Create emblem_data.js

---
+++ emblem_data.js
@@ -0,0 +1,7 @@
+var emblem_data = {
+    emblems: [
+        ["Administrator", "rgb(16, 37, 63)", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-750px 0px", "0", "http://b3.ifrm.com/30609/91/0/p3003840/emblemdesigns_tangerine.png", "-450px 0px", "0"],
+        ["Daniel Terrington", "rgb(189, 214, 26)", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-150px 0px", "270", "http://b3.ifrm.com/30609/91/0/p3003819/emblemdesigns_azure.png", "-300px -150px", "270"]
+    ]
+}
+
