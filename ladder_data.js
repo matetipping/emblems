7d8996a6c59815fc1d89a20339772133f5a9aac0
matetipping <matetipping@gmail.com>
2017-02-07 17:28:47 +1100

    Create ladder_data.js

---
+++ ladder_data.js
@@ -0,0 +1,6 @@
+var ladder_data = {
+    tips: [
+        ["Administrator", "GWS", "WBD", "SYD", "ADE", "GEE", "HAW", "MEL", "STK", "WCE", "CAR", "PTA", "ESS", "COL", "RIC", "GCS", "NTH", "FRE", "BRI"],
+        []
+    ]
+}
