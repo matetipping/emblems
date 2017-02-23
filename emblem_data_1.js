b4200932b9be0f5dd33ee11ec0a737a283152455
matetipping <matetipping@gmail.com>
2017-02-23 21:59:11 +1100

    Update and rename emblem_data_1.js to emblem_data_2.js

--- emblem_data_1.js
+++
@@ -1,7 +0,0 @@
-var emblem_data = {
-    emblems: [
-        ["Administrator", "rgb(16, 37, 63)", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-750px 0px", "0", "http://b3.ifrm.com/30609/91/0/p3003840/emblemdesigns_tangerine.png", "-450px 0px", "0"],
-        ["Dannelboyz", "rgb(16, 37, 63)", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-750px 0px", "0", "http://b3.ifrm.com/30609/91/0/p3003814/emblemdesigns_lime.png", "-150px -450px", "0"]
-    ]
-}
-
---
+++ emblem_data_2.js
@@ -0,0 +1,8 @@
+var emblem_data = {
+    emblems: [
+        ["Administrator", "rgb(16, 37, 63)", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-750px 0px", "0", "http://b3.ifrm.com/30609/91/0/p3003840/emblemdesigns_tangerine.png", "-450px 0px", "0"],
+        ["Dannelboyz", "rgb(16, 37, 63)", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-750px 0px", "0", "http://b3.ifrm.com/30609/91/0/p3003814/emblemdesigns_lime.png", "-150px -450px", "0"],
+        ["Jono22", "rgb(13, 13, 13)", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-450px -150px", "0", "http://b3.ifrm.com/30609/91/0/p3005760/emblemdesigns_carrot.png", "-600px -150px", "0"]
+    ]
+}
+
