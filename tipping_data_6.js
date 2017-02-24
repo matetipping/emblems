a310e9a8f07b13c39757c4e240baaef904f01c4b
matetipping <matetipping@gmail.com>
2017-02-25 08:54:35 +1100

    Update and rename tipping_data_5.js to tipping_data_6.js

--- tipping_data_6.js
+++ tipping_data_6.js
@@ -3,7 +3,9 @@ var tipping_data = {
     admin: "Administrator",
     tips: [
         ["R1", "Administrator", "", ""],
-        ["R1", "Shaydog", "CAR", 15, "WBD", 25, "STK", 10, "SYD", 45, "BRI", 20, "HAW", 30, "NTH", 0, "ADE", 15, "GEE", 25, "", ""]
+        ["R1", "Shaydog", "CAR", 15, "WBD", 25, "STK", 10, "SYD", 45, "BRI", 20, "HAW", 30, "NTH", 0, "ADE", 15, "GEE", 25, "", ""],
+        ["R1", "Dannelboyz", "CAR", 6, "WBD", 18, "STK", 2, "SYD", 52, "BRI", 7, "HAW", 29, "WCE", 42, "GWS", 19, "GEE", 17, "", "Josh Kennedy (WCE)"],
+        ["Bigbadbrucey", "rgb(56, 249, 109)", "http://b3.ifrm.com/30609/91/0/p3005725/emblemdesigns_charcoalbrown.png", "-600px 0px", "0", "none", "-750px -300px", "0"]
     ],
     fixtures: [
         ["R1", "Administrator", "Shaydog"],
