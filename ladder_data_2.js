ed4be7d64b9705b5c696dcdf908915b7ff1549d5
matetipping <matetipping@gmail.com>
2017-02-24 14:20:58 +1100

    Update and rename ladder_data_1.js to ladder_data_2.js

--- ladder_data_2.js
+++ ladder_data_2.js
@@ -1,6 +1,6 @@
 var ladder_data = {
     tips: [
-        ["Administrator", "GWS", "WBD", "SYD", "ADE", "GEE", "HAW", "MEL", "STK", "WCE", "CAR", "PTA", "ESS", "COL", "RIC", "GCS", "NTH", "FRE", "BRI"],
+        ["Administrator"],
         ["Dannelboyz", "GWS", "ADE", "SYD", "GEE", "WBD", "STK", "MEL", "HAW", "FRE", "WCE", "ESS", "CAR", "PTA", "COL", "RIC", "BRI", "GCS", "NTH"]
     ]
 }
