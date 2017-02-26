d18b510dbe8d75531a1805c6107aa9764153ef24
matetipping <matetipping@gmail.com>
2017-02-26 18:16:13 +1100

    Update and rename ladderprediction_data_2.js to ladderprediction_data_3.js

--- ladderprediction_data_2.js
+++
@@ -1,6 +0,0 @@
-var ladder_data = {
-    tips: [
-        ["Dannelboyz", "GWS", "ADE", "SYD", "GEE", "WBD", "STK", "MEL", "HAW", "FRE", "WCE", "ESS", "CAR", "PTA", "COL", "RIC", "BRI", "GCS", "NTH"],
-        ["Bigbadbrucey", "GWS", "ADE", "SYD", "WBD", "STK", "GEE", "MEL", "HAW", "FRE", "WCE", "RIC", "GCS", "ESS", "COL", "PTA", "CAR", "NTH", "BRI"]
-    ]
-}
