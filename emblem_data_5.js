ef46e0f4b957430611b975a94e8d6ed6b0c8e529
matetipping <matetipping@gmail.com>
2017-02-27 20:42:24 +1100

    Update emblem_data_5.js

--- emblem_data_5.js
+++ emblem_data_5.js
@@ -4,7 +4,8 @@ var emblem_data = {
         ["Dannelboyz", "rgb(16, 37, 63)", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-750px 0px", "0", "http://b3.ifrm.com/30609/91/0/p3003814/emblemdesigns_lime.png", "-150px -450px", "0"],
         ["Jono22", "rgb(13, 13, 13)", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-450px -150px", "0", "http://b3.ifrm.com/30609/91/0/p3005760/emblemdesigns_carrot.png", "-600px -150px", "0"],
         ["Bigbadbrucey", "rgb(56, 249, 109)", "http://b3.ifrm.com/30609/91/0/p3005725/emblemdesigns_charcoalbrown.png", "-600px 0px", "0", "none", "-750px -300px", "0"],
-        ["ciniboi_12", "rgb(158, 211, 131)", "http://b3.ifrm.com/30609/91/0/p3003840/emblemdesigns_tangerine.png", "-150px -300px", "0", "http://b3.ifrm.com/30609/91/0/p3003819/emblemdesigns_azure.png", "-450px -300px", "0"]
+        ["ciniboi_12", "rgb(158, 211, 131)", "http://b3.ifrm.com/30609/91/0/p3003840/emblemdesigns_tangerine.png", "-150px -300px", "0", "http://b3.ifrm.com/30609/91/0/p3003819/emblemdesigns_azure.png", "-450px -300px", "0"],
+        ["Craig Terrington", "rgb(128, 28, 90)", "http://b3.ifrm.com/30609/91/0/p3003822/emblemdesigns_glaucous.png", "-600px 0px", "90", "http://b3.ifrm.com/30609/91/0/p3005722/emblemdesigns_lilac.png", "-150px -450px", "180"]
     ]
 }
 
