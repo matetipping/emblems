6bc90415edc5523407fe4b61474c3030798b8d24
matetipping <matetipping@gmail.com>
2017-02-27 22:54:24 +1100

    Update and rename tipping_data_8.js to tipping_data_10.js

--- tipping_data_10.js
+++ tipping_data_10.js
@@ -6,7 +6,8 @@ var tipping_data = {
         ["R1", "Shaydog", "CAR", 15, "WBD", 25, "STK", 10, "SYD", 45, "BRI", 20, "HAW", 30, "NTH", 0, "ADE", 15, "GEE", 25, "", ""],
         ["R1", "Dannelboyz", "CAR", 6, "WBD", 18, "STK", 2, "SYD", 52, "BRI", 7, "HAW", 29, "WCE", 42, "GWS", 19, "GEE", 17, "", "Josh Kennedy (WCE)"],
         ["R1", "Bigbadbrucey", "RIC", 23, "WBD", 37, "STK", 17, "SYD", 43, "GCS", 22, "HAW", 37, "WCE", 27, "ADE", 12, "GEE", 28, "", ""],
-        ["R1", "Flagpies", "RIC", 25, "WBD", 19, "MEL", 25, "SYD", 12, "GCS", 8, "HAW", 15, "WCE", 22, "GWS", 10, "GEE", 4, "Dan Hannebery (SYD)", "Travis Cloke (WBD)"]
+        ["R1", "Flagpies", "RIC", 25, "WBD", 19, "MEL", 25, "SYD", 12, "GCS", 8, "HAW", 15, "WCE", 22, "GWS", 10, "GEE", 4, "Dan Hannebery (SYD)", "Travis Cloke (WBD)"],
+        ["R1", "Jono22", "CAR", 15, "WBD", 27, "STK", 4, "SYD", 21, "GCS", 13, "HAW", 21, "WCE", 17, "GWS", 19, "FRE", 13, "Tom Mitchell (HAW)", ""]
     ],
     fixtures: [
         ["R1", "Shaydog", "Dannelboyz", "Bigbadbrucey", "Flagpies"],
