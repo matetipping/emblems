892c31555a7141d3bbbaf3ff5f6685b68ce818fe
matetipping <matetipping@gmail.com>
2017-02-27 22:29:28 +1100

    Update and rename emblem_data_7.js to emblem_data_8.js

--- emblem_data_8.js
+++ emblem_data_8.js
@@ -7,7 +7,8 @@ var emblem_data = {
         ["ciniboi_12", "rgb(158, 211, 131)", "http://b3.ifrm.com/30609/91/0/p3003840/emblemdesigns_tangerine.png", "-150px -300px", "0", "http://b3.ifrm.com/30609/91/0/p3003819/emblemdesigns_azure.png", "-450px -300px", "0"],
         ["Craig Terrington", "rgb(128, 28, 90)", "http://b3.ifrm.com/30609/91/0/p3003822/emblemdesigns_glaucous.png", "-600px 0px", "90", "http://b3.ifrm.com/30609/91/0/p3005722/emblemdesigns_lilac.png", "-150px -450px", "180"],
         ["Flagpies", "rgb(151, 13, 36)", "http://b3.ifrm.com/30609/91/0/p3005772/emblemdesigns_royalblue.png", "-150px 0px", "270", "http://b3.ifrm.com/30609/91/0/p3003804/emblemdesigns_amber.png", "none", "0"],
-        ["Mann", "rgb(148, 8, 8)", "http://b3.ifrm.com/30609/91/0/p3003804/emblemdesigns_amber.png", "-600px -300px", "0", "http://b3.ifrm.com/30609/91/0/p3003804/emblemdesigns_amber.png", "-450px 0px", "0"]
+        ["Mann", "rgb(148, 8, 8)", "http://b3.ifrm.com/30609/91/0/p3003804/emblemdesigns_amber.png", "-600px -300px", "0", "http://b3.ifrm.com/30609/91/0/p3003804/emblemdesigns_amber.png", "-450px 0px", "0"],
+        ["Shaydog", "rgb(13, 13, 13)", "http://b3.ifrm.com/30609/91/0/p3003806/emblemdesigns_maroon.png", "-450px -150px", "270", "http://b3.ifrm.com/30609/91/0/p3003751/emblemdesigns_black.png", "-600px -150px", "180"]
     ]
 }
 
