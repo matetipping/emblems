d2a6bdeb508f64c9fb542fdc5e54e4a189b35004
matetipping <matetipping@gmail.com>
2017-02-27 20:42:52 +1100

    Update ladderprediction_data_3.js

--- ladderprediction_data_3.js
+++ ladderprediction_data_3.js
@@ -2,6 +2,8 @@ var ladder_data = {
     tips: [
         ["Dannelboyz", "GWS", "ADE", "SYD", "GEE", "WBD", "STK", "MEL", "HAW", "FRE", "WCE", "ESS", "CAR", "PTA", "COL", "RIC", "BRI", "GCS", "NTH"],
         ["Bigbadbrucey", "GWS", "ADE", "SYD", "WBD", "STK", "GEE", "MEL", "HAW", "FRE", "WCE", "RIC", "GCS", "ESS", "COL", "PTA", "CAR", "NTH", "BRI"],
-        ["ciniboi_12", "SYD", "GWS", "GEE", "WBD", "ADE", "WCE", "HAW", "STK", "MEL", "COL", "RIC", "CAR", "FRE", "GCS", "PTA", "ESS", "NTH", "BRI"]
+        ["ciniboi_12", "SYD", "GWS", "GEE", "WBD", "ADE", "WCE", "HAW", "STK", "MEL", "COL", "RIC", "CAR", "FRE", "GCS", "PTA", "ESS", "NTH", "BRI"],
+        ["Craig Terrington", "GWS", "ADE", "WCE", "FRE", "HAW", "WBD", "SYD", "NTH", "MEL", "STK", "PTA", "GEE", "ESS", "GCS", "RIC", "CAR", "COL", "BRI"]
+
     ]
 }
