907f62c3a359f3a2a0b32dcfa727254098878e18
matetipping <matetipping@gmail.com>
2017-03-13 18:39:40 +1100

    Update and rename ladderprediction_data_9.js to ladderprediction_data_10.js

--- ladderprediction_data_10.js
+++ ladderprediction_data_10.js
@@ -9,6 +9,7 @@ var ladder_data = {
         ["Jono22", "GWS", "WBD", "SYD", "HAW", "GEE", "ADE", "STK", "MEL", "PTA", "RIC", "GCS", "COL", "FRE", "ESS", "WCE", "BRI", "NTH", "CAR"],
         ["Alex Rowland", "GWS", "SYD", "ADE", "GEE", "WCE", "WBD", "STK", "HAW", "MEL", "COL", "ESS", "FRE", "GCS", "RIC", "CAR", "PTA", "NTH", "BRI"],
         ["Mann", "GWS", "SYD", "WCE", "GEE", "WBD", "ADE", "HAW", "MEL", "STK", "PTA", "FRE", "GCS", "COL", "ESS", "RIC", "NTH", "CAR", "BRI"],
-        ["Steve", "GWS", "SYD", "GEE", "WBD", "ADE", "HAW", "MEL", "STK", "RIC", "COL", "WCE", "GCS", "BRI", "FRE", "PTA", "NTH", "ESS", "CAR"]
+        ["Steve", "GWS", "SYD", "GEE", "WBD", "ADE", "HAW", "MEL", "STK", "RIC", "COL", "WCE", "GCS", "BRI", "FRE", "PTA", "NTH", "ESS", "CAR"],
+        ["Harry Rowland", "GWS", "WBD", "GEE", "HAW", "SYD", "ADE", "STK", "GCS", "NTH", "COL", "PTA", "RIC", "WCE", "CAR", "MEL", "BRI", "FRE", "ESS"]
     ]
 }
