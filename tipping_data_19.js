d60b386acee30f4b8b25a281ff5b47e7655da94d
matetipping <matetipping@gmail.com>
2017-03-17 09:49:02 +1100

    Update and rename tipping_data_18.js to tipping_data_19.js

--- tipping_data_19.js
+++ tipping_data_19.js
@@ -2,8 +2,8 @@ var tipping_data = {
     round: "R1",
     admin: "Administrator",
     tips: [
-        ["R1", "Administrator", "CAR", 20, "WBD", 25, "", ""],
-        ["R1", "Shaydog", "CAR", 15, "WBD", 25, "STK", 10, "SYD", 45, "BRI", 20, "HAW", 30, "NTH", 0, "ADE", 15, "GEE", 25, "", "", "", "", "", ""],
+        ["R1", "Administrator", "CAR", 20, "WBD", 25, "STK", 2, "SYD", 49, "BRI", 6, "HAW", 13, "WCE", 37, "GWS", 29, "FRE", 3, "", "", "", "", "", ""],
+        ["R1", "Shaydog", "CAR", 15, "WBD", 25, "STK", 10, "SYD", 45, "BRI", 20, "HAW", 30, "NTH", 10, "ADE", 15, "GEE", 25, "", "", "", "", "", ""],
         ["R1", "Dannelboyz", "CAR", 6, "WBD", 18, "STK", 2, "SYD", 52, "BRI", 7, "HAW", 29, "WCE", 42, "GWS", 19, "GEE", 17, "", "", "Josh Kennedy (WCE)", "", "", ""],
         ["R1", "Bigbadbrucey", "RIC", 23, "WBD", 37, "STK", 17, "SYD", 43, "GCS", 22, "HAW", 37, "WCE", 27, "ADE", 12, "GEE", 28, "", "", "", "", "", ""],
         ["R1", "Flagpies", "RIC", 25, "WBD", 19, "MEL", 25, "SYD", 12, "GCS", 8, "HAW", 15, "WCE", 22, "GWS", 10, "GEE", 4, "Dan Hannebery (SYD)", "", "Travis Cloke (WBD)", "", "", ""],
