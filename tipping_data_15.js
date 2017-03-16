2e8bf68c6b7a998993c3155956a09f4e8fb59079
matetipping <matetipping@gmail.com>
2017-03-16 22:34:46 +1100

    Update and rename tipping_data_14.js to tipping_data_15.js

---
+++ tipping_data_15.js
@@ -0,0 +1,37 @@
+var tipping_data = {
+    round: "R1",
+    admin: "Administrator",
+    tips: [
+        ["R1", "Administrator", "CAR", 20, "WBD", 25, "", ""],
+        ["R1", "Shaydog", "CAR", 15, "WBD", 25, "STK", 10, "SYD", 45, "BRI", 20, "HAW", 30, "NTH", 0, "ADE", 15, "GEE", 25, "", ""],
+        ["R1", "Dannelboyz", "CAR", 6, "WBD", 18, "STK", 2, "SYD", 52, "BRI", 7, "HAW", 29, "WCE", 42, "GWS", 19, "GEE", 17, "", "Josh Kennedy (WCE)"],
+        ["R1", "Bigbadbrucey", "RIC", 23, "WBD", 37, "STK", 17, "SYD", 43, "GCS", 22, "HAW", 37, "WCE", 27, "ADE", 12, "GEE", 28, "", ""],
+        ["R1", "Flagpies", "RIC", 25, "WBD", 19, "MEL", 25, "SYD", 12, "GCS", 8, "HAW", 15, "WCE", 22, "GWS", 10, "GEE", 4, "Dan Hannebery (SYD)", "Travis Cloke (WBD)"],
+        ["R1", "Jono22", "CAR", 15, "WBD", 27, "STK", 4, "SYD", 21, "GCS", 13, "HAW", 21, "WCE", 17, "GWS", 19, "FRE", 13, "Tom Mitchell (HAW)", ""],
+        ["R1", "Kaytley🐐", "RIC", 10, "WBD", 15, "STK", 20, "SYD", 12, "BRI", 10, "HAW", 20, "WCE", 25, "ADE", 10, "FRE", 15, "", ""]
+    ],
+    fixtures: [
+        ["R1", "Shaydog", "Dannelboyz", "Bigbadbrucey", "Flagpies", "Jono22", "ciniboi_12", "Craig Terrington", "Alex Rowland", "Mann", "Steve", "Harry Rowland", "Kaytley🐐", "jamieling", "Jeanille01"],
+        ["R2", "Dannelboyz", "Jeanille01", "Kaytley🐐", "jamieling", "Steve", "Harry Rowland", "Alex Rowland", "Mann", "ciniboi_12", "Craig Terrington", "Flagpies", "Jono22", "Shaydog", "Bigbadbrucey"],
+        ["R3", "Bigbadbrucey", "Dannelboyz", "Jono22", "Shaydog", "Craig Terrington", "Flagpies", "Mann", "ciniboi_12", "Harry Rowland", "Alex Rowland", "jamieling", "Steve", "Jeanille01", "Kaytley🐐"],
+        ["R4", "Dannelboyz", "Kaytley🐐", "Steve", "Jeanille01", "Alex Rowland", "jamieling", "ciniboi_12", "Harry Rowland", "Flagpies", "Mann", "Shaydog", "Craig Terrington", "Bigbadbrucey", "Jono22"],
+        ["R5", "Jono22", "Dannelboyz", "Craig Terrington", "Bigbadbrucey", "Mann", "Shaydog", "Harry Rowland", "Flagpies", "jamieling", "ciniboi_12", "Jeanille01", "Alex Rowland", "Kaytley🐐", "Steve"],
+        ["R6", "Dannelboyz", "Steve", "Alex Rowland", "Kaytley🐐", "ciniboi_12", "Jeanille01", "Flagpies", "jamieling", "Shaydog", "Harry Rowland", "Bigbadbrucey", "Mann", "Jono22", "Craig Terrington"],
+        ["R7", "Craig Terrington", "Dannelboyz", "Mann", "Jono22", "Harry Rowland", "Bigbadbrucey", "jamieling", "Shaydog", "Jeanille01", "Flagpies", "Kaytley🐐", "ciniboi_12", "Steve", "Alex Rowland"],
+        ["R8", "Dannelboyz", "Alex Rowland", "ciniboi_12", "Steve", "Flagpies", "Kaytley🐐", "Shaydog", "Jeanille01", "Bigbadbrucey", "jamieling", "Jono22", "Harry Rowland", "Craig Terrington", "Mann"],
+        ["R9", "Mann", "Dannelboyz", "Harry Rowland", "Craig Terrington", "jamieling", "Jono22", "Jeanille01", "Bigbadbrucey", "Kaytley🐐", "Shaydog", "Steve", "Flagpies", "Alex Rowland", "ciniboi_12"],
+        ["R10", "Dannelboyz", "ciniboi_12", "Flagpies", "Alex Rowland", "Shaydog", "Steve", "Bigbadbrucey", "Kaytley🐐", "Jono22", "Jeanille01", "Craig Terrington", "jamieling", "Mann", "Harry Rowland"],
+        ["B1", "Harry Rowland", "Dannelboyz", "jamieling", "Mann", "Jeanille01", "Craig Terrington", "Kaytley🐐", "Jono22", "Steve", "Bigbadbrucey", "Alex Rowland", "Shaydog", "ciniboi_12", "Flagpies"],
+        ["B2", "Dannelboyz", "Flagpies", "Shaydog", "ciniboi_12", "Bigbadbrucey", "Alex Rowland", "Jono22", "Steve", "Craig Terrington", "Kaytley🐐", "Mann", "Jeanille01", "Harry Rowland", "jamieling"],
+        ["R14", "jamieling", "Dannelboyz", "Jeanille01", "Harry Rowland", "Kaytley🐐", "Mann", "Steve", "Craig Terrington", "Alex Rowland", "Jono22", "ciniboi_12", "Bigbadbrucey", "Flagpies", "Shaydog"],
+        ["R15", "Shaydog", "Dannelboyz", "Bigbadbrucey", "Flagpies", "Jono22", "ciniboi_12", "Craig Terrington", "Alex Rowland", "Mann", "Steve", "Harry Rowland", "Kaytley🐐", "jamieling", "Jeanille01"],
+        ["R16", "Dannelboyz", "Jeanille01", "Kaytley🐐", "jamieling", "Steve", "Harry Rowland", "Alex Rowland", "Mann", "ciniboi_12", "Craig Terrington", "Flagpies", "Jono22", "Shaydog", "Bigbadbrucey"],
+        ["R17", "Bigbadbrucey", "Dannelboyz", "Jono22", "Shaydog", "Craig Terrington", "Flagpies", "Mann", "ciniboi_12", "Harry Rowland", "Alex Rowland", "jamieling", "Steve", "Jeanille01", "Kaytley🐐"],
+        ["R18", "Dannelboyz", "Kaytley🐐", "Steve", "Jeanille01", "Alex Rowland", "jamieling", "ciniboi_12", "Harry Rowland", "Flagpies", "Mann", "Shaydog", "Craig Terrington", "Bigbadbrucey", "Jono22"],
+        ["R19", "Jono22", "Dannelboyz", "Craig Terrington", "Bigbadbrucey", "Mann", "Shaydog", "Harry Rowland", "Flagpies", "jamieling", "ciniboi_12", "Jeanille01", "Alex Rowland", "Kaytley🐐", "Steve"],
+        ["R20", "Dannelboyz", "Steve", "Alex Rowland", "Kaytley🐐", "ciniboi_12", "Jeanille01", "Flagpies", "jamieling", "Shaydog", "Harry Rowland", "Bigbadbrucey", "Mann", "Jono22", "Craig Terrington"],
+        ["R21", "Craig Terrington", "Dannelboyz", "Mann", "Jono22", "Harry Rowland", "Bigbadbrucey", "jamieling", "Shaydog", "Jeanille01", "Flagpies", "Kaytley🐐", "ciniboi_12", "Steve", "Alex Rowland"],
+        ["R22", "Dannelboyz", "Alex Rowland", "ciniboi_12", "Steve", "Flagpies", "Kaytley🐐", "Shaydog", "Jeanille01", "Bigbadbrucey", "jamieling", "Jono22", "Harry Rowland", "Craig Terrington", "Mann"],
+        ["R23", "Mann", "Dannelboyz", "Harry Rowland", "Craig Terrington", "jamieling", "Jono22", "Jeanille01", "Bigbadbrucey", "Kaytley🐐", "Shaydog", "Steve", "Flagpies", "Alex Rowland", "ciniboi_12"]
+    ]
+}
