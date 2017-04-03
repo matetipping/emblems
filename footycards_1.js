b97a80d240171bdebc62c214686e2cf8036074ec
matetipping <matetipping@gmail.com>
2017-04-03 20:27:30 +1000

    Update and rename footycards.js to footycards_1.js

--- footycards_1.js
+++ footycards_1.js
@@ -5,6 +5,7 @@ var t_award = {
   closeFunction : "fade",
   users : [
     [3417755,"45 - Jarryd Roughead","http://b3.ifrm.com/30609/91/0/p3005900/card_45_j_roughead.png","Hawthorn Hawks","2017"],
-    [3417755,"49 - Jordan Lewis","http://b3.ifrm.com/30609/91/0/p3005904/card_49_j_lewis.png","Melbourne Demons","2017"]
+    [3417755,"49 - Jordan Lewis","http://b3.ifrm.com/30609/91/0/p3005904/card_49_j_lewis.png","Melbourne Demons","2017"],
+    [3465693,"57 - Chad Wingard","http://b3.ifrm.com/30609/91/0/p3005912/card_57_c_wingard.png","Port Adelaide","2017"]
   ]
 };
