cf6c928e77f4eefc44e6332e12b67671f7a7fb11
matetipping <matetipping@gmail.com>
2017-04-03 20:12:59 +1000

    Create footycards.js

---
+++ footycards.js
@@ -0,0 +1,10 @@
+// Created by Viral of Viral's Modifications - http://viralsmods.com
+var t_award = {
+  name : "Footy Cards",
+  thumbnail : [20, 20],
+  closeFunction : "fade",
+  users : [
+    [3417755,"45 - Jarryd Roughead","http://b3.ifrm.com/30609/91/0/p3005900/card_45_j_roughead.png","Hawthorn Hawks","2017"],
+    [3417755,"49 - Jordan Lewis","http://b3.ifrm.com/30609/91/0/p3005904/card_49_j_lewis.png","Melbourne Demons","2017"]
+  ]
+};
