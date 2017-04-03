349e9bc66a8cf496b05349ab1ef8c74e9ae5d933
matetipping <matetipping@gmail.com>
2017-04-03 20:28:37 +1000

    Update and rename footycards_1.js to footycards_2.js

--- footycards_2.js
+++ footycards_2.js
@@ -4,6 +4,7 @@ var t_award = {
   thumbnail : [20, 20],
   closeFunction : "fade",
   users : [
+    [3465693,"28 - David Mundy","http://b3.ifrm.com/30609/91/0/p3005883/card_28_d_mundy.png","Fremantle Dockers","2017"],
     [3417755,"45 - Jarryd Roughead","http://b3.ifrm.com/30609/91/0/p3005900/card_45_j_roughead.png","Hawthorn Hawks","2017"],
     [3417755,"49 - Jordan Lewis","http://b3.ifrm.com/30609/91/0/p3005904/card_49_j_lewis.png","Melbourne Demons","2017"],
     [3465693,"57 - Chad Wingard","http://b3.ifrm.com/30609/91/0/p3005912/card_57_c_wingard.png","Port Adelaide","2017"]
