4b8a4afb4b9c639121fbb3176f227b4510a82379
matetipping <matetipping@gmail.com>
2017-04-04 16:05:14 +1000

    Update and rename footycards_4.js to footycards_5.js

--- footycards_5.js
+++ footycards_5.js
@@ -6,6 +6,7 @@ var t_award = {
   users : [
     [3465693,"13 - Dayne Beams","http://b3.ifrm.com/30609/91/0/p3005868/card_13_d_beams.png","Brisbane Lions","2017"],
     [3465693,"28 - David Mundy","http://b3.ifrm.com/30609/91/0/p3005883/card_28_d_mundy.png","Fremantle Dockers","2017"],
+    [5061544,"40 - Heath Shaw","http://b3.ifrm.com/30609/91/0/p3005895/card_40_h_shaw.png","GWS Giants","2017"],
     [3417755,"45 - Jarryd Roughead","http://b3.ifrm.com/30609/91/0/p3005900/card_45_j_roughead.png","Hawthorn Hawks","2017"],
     [3417755,"49 - Jordan Lewis","http://b3.ifrm.com/30609/91/0/p3005904/card_49_j_lewis.png","Melbourne Demons","2017"],
     [3465693,"57 - Chad Wingard","http://b3.ifrm.com/30609/91/0/p3005912/card_57_c_wingard.png","Port Adelaide","2017"]
