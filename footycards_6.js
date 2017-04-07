bc4d53e3c5404a94fa4ad0ac0ca40875f7cbcbc0
matetipping <matetipping@gmail.com>
2017-04-07 23:29:37 +1000

    Update and rename footycards_5.js to footycards_6.js

--- footycards_6.js
+++ footycards_6.js
@@ -9,6 +9,7 @@ var t_award = {
     [5061544,"40 - Heath Shaw","http://b3.ifrm.com/30609/91/0/p3005895/card_40_h_shaw.png","GWS Giants","2017"],
     [3417755,"45 - Jarryd Roughead","http://b3.ifrm.com/30609/91/0/p3005900/card_45_j_roughead.png","Hawthorn Hawks","2017"],
     [3417755,"49 - Jordan Lewis","http://b3.ifrm.com/30609/91/0/p3005904/card_49_j_lewis.png","Melbourne Demons","2017"],
-    [3465693,"57 - Chad Wingard","http://b3.ifrm.com/30609/91/0/p3005912/card_57_c_wingard.png","Port Adelaide","2017"]
+    [3465693,"57 - Chad Wingard","http://b3.ifrm.com/30609/91/0/p3005912/card_57_c_wingard.png","Port Adelaide","2017"],
+    [3465669,"91 - Lance Franklin (250 Games)","http://b3.ifrm.com/30609/91/0/p3009200/card_91_l_franklin.png","Sydney Swans","2017"]
   ]
 };
