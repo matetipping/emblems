d4c9fc68f01812f33b52cf2f1dbfbd2fc5989707
matetipping <matetipping@gmail.com>
2017-04-09 18:02:37 +1000

    Update and rename footycards_6.js to footycards_7.js

--- footycards_7.js
+++ footycards_7.js
@@ -6,10 +6,12 @@ var t_award = {
   users : [
     [3465693,"13 - Dayne Beams","http://b3.ifrm.com/30609/91/0/p3005868/card_13_d_beams.png","Brisbane Lions","2017"],
     [3465693,"28 - David Mundy","http://b3.ifrm.com/30609/91/0/p3005883/card_28_d_mundy.png","Fremantle Dockers","2017"],
+    [3465706,"31 - Joel Selwood","http://b3.ifrm.com/30609/91/0/p3005886/card_31_j_selwood.png","Geelong Cats","2017"],
     [5061544,"40 - Heath Shaw","http://b3.ifrm.com/30609/91/0/p3005895/card_40_h_shaw.png","GWS Giants","2017"],
     [3417755,"45 - Jarryd Roughead","http://b3.ifrm.com/30609/91/0/p3005900/card_45_j_roughead.png","Hawthorn Hawks","2017"],
     [3417755,"49 - Jordan Lewis","http://b3.ifrm.com/30609/91/0/p3005904/card_49_j_lewis.png","Melbourne Demons","2017"],
     [3465693,"57 - Chad Wingard","http://b3.ifrm.com/30609/91/0/p3005912/card_57_c_wingard.png","Port Adelaide","2017"],
-    [3465669,"91 - Lance Franklin (250 Games)","http://b3.ifrm.com/30609/91/0/p3009200/card_91_l_franklin.png","Sydney Swans","2017"]
+    [3465669,"91 - Lance Franklin (250 Games)","http://b3.ifrm.com/30609/91/0/p3009200/card_91_l_franklin.png","Sydney Swans","2017"],
+    [3465706,"91 - Lance Franklin (250 Games)","http://b3.ifrm.com/30609/91/0/p3009200/card_91_l_franklin.png","Sydney Swans","2017"]
   ]
 };
