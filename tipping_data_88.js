d9f473c9a3b74dca58ac671b8ef097ea8b822e4c
matetipping <matetipping@gmail.com>
2017-05-14 16:26:22 +1000

    Update and rename tipping_data_87.js to tipping_data_88.js

--- tipping_data_88.js
+++ tipping_data_88.js
@@ -2,7 +2,7 @@ var tipping_data = {
     round: "R8",
     admin: "Administrator",
     tips: [
-        ["R8", "Administrator", "WCE", 8, "HAW", 38, "STK", 19, "GWS", 3, "ESS", 17, "MEL", 41, "", "", "", "", "", ""],
+        ["R8", "Administrator", "WCE", 8, "HAW", 38, "STK", 19, "GWS", 3, "ESS", 17, "MEL", 41, "FRE", 2, "", "", "", "", "", ""],
         ["R1", "Administrator", "RIC", 43, "WBD", 14, "MEL", 30, "PTA", 28, "BRI", 2, "ESS", 25, "WCE", 43, "ADE", 56, "GEE", 42, "Rory Laird (ADE)", 40, "Josh Kennedy (WCE)", 42, "", ""],
         ["R1", "Shaydog", "CAR", 15, "WBD", 25, "STK", 10, "SYD", 45, "BRI", 20, "HAW", 30, "NTH", 10, "ADE", 15, "GEE", 25, "", "", "", "", "", ""],
         ["R1", "Dannelboyz", "CAR", 6, "WBD", 18, "STK", 2, "SYD", 52, "BRI", 7, "HAW", 29, "WCE", 42, "GWS", 19, "GEE", 17, "", "", "Josh Kennedy (WCE)", 42, "", ""],
@@ -108,7 +108,7 @@ var tipping_data = {
         ["R7", "Shaydog", "GWS", 28, "ADE", 32, "COL", 22, "PTA", 7, "GEE", 26, "RIC", 12, "BRI", 16, "MEL", 10, "ESS", 8, "Scott Pendlebury (COL)", 29, "", "", "", ""],
         ["R7", "Alex Rowland", "GWS", 27, "ADE", 40, "COL", 16, "PTA", 31, "GEE", 35, "WBD", 25, "SYD", 22, "MEL", 18, "FRE", 0, "Tom Mitchell (HAW)", 31, "", "", "", ""],
         ["R7", "ciniboi_12", "GWS", 33, "ADE", 45, "CAR", 5, "WCE", 13, "GEE", 55, "WBD", 17, "SYD", 29, "MEL", 24, "FRE", 3, "", "", "", "", "", ""],
-        ["R8", "jamieling", "WCE", 17, "HAW", 38, "STK", 33, "GWS", 40, "GEE", 20, "ADE", 45, "RIC", 22, "PTA", 29, "SYD", 13, "", "", "Taylor Walker (ADE)", "", "", ""],
+        ["R8", "jamieling", "WCE", 17, "HAW", 38, "STK", 33, "GWS", 40, "GEE", 20, "ADE", 45, "RIC", 22, "PTA", 29, "SYD", 13, "", "", "Taylor Walker (ADE)", 14, "", ""],
         ["R8", "Dannelboyz", "WCE", 18, "HAW", 25, "STK", 36, "GWS", 45, "GEE", 19, "ADE", 27, "RIC", 15, "GCS", 6, "NTH", 12, "", "", "", "", "", ""],
         ["R8", "Bigbadbrucey", "WBD", 5, "HAW", 30, "STK", 40, "GWS", 30, "GEE", 30, "ADE", 45, "RIC", 15, "GCS", 20, "NTH", 5, "Jack Steven (STK)", 32, "Tom Lynch (GCS)", "", "", ""],
         ["R8", "Jono22", "WCE", 21, "HAW", 21, "STK", 21, "GWS", 21, "GEE", 21, "ADE", 21, "RIC", 21, "GCS", 21, "SYD", 21, "", "", "", "", "", ""],
@@ -118,7 +118,7 @@ var tipping_data = {
         ["R8", "Steve", "WCE", 15, "HAW", 10, "STK", 38, "GWS", 44, "GEE", 18, "ADE", 32, "RIC", 9, "PTA", 19, "SYD", 48, "Patrick Dangerfield (GEE)", 30, "Lance Franklin (SYD)", "", "", ""],
         ["R8", "Craig Terrington", "WBD", 1, "HAW", 29, "STK", 32, "GWS", 54, "GEE", 33, "ADE", 41, "RIC", 18, "PTA", 35, "SYD", 2, "", "", "", "", "", ""],
         ["R8", "Flagpies", "WCE", 16, "HAW", 24, "CAR", 18, "GWS", 22, "GEE", 19, "ADE", 10, "RIC", 22, "GCS", 20, "NTH", 30, "", "", "", "", "", ""],
-        ["R8", "Kaytley🐐", "WCE", 15, "HAW", 10, "STK", 10, "GWS", 10, "ESS", 5, "ADE", 20, "RIC", 25, "PTA", 10, "SYD", 15, "Rory Sloane (ADE)", "", "Josh Kennedy (SYD)", "", "", ""],
+        ["R8", "Kaytley🐐", "WCE", 15, "HAW", 10, "STK", 10, "GWS", 10, "ESS", 5, "ADE", 20, "RIC", 25, "PTA", 10, "SYD", 15, "Rory Sloane (ADE)", 11, "Josh Kennedy (SYD)", "", "", ""],
         ["R8", "Alex Rowland", "WBD", 15, "HAW", 41, "STK", 22, "GWS", 32, "GEE", 25, "ADE", 27, "RIC", 16, "PTA", 18, "NTH", 11, "", "", "", "", "", ""],
         ["R8", "Shaydog", "WBD", 15, "BRI", 23, "STK", 19, "GWS", 20, "ESS", 5, "ADE", 32, "RIC", 12, "GCS", 15, "NTH", 10, "", "", "Tom Rockliff (BRI)", 6, "", ""],
         ["R8", "ciniboi_12", "WCE", 5, "HAW", 21, "STK", 44, "GWS", 19, "GEE", 24, "ADE", 15, "RIC", 9, "PTA", 36, "SYD", 22, "", "", "Tim Membrey (STK)", 7, "", ""]
