37c806ffb7b4992bb1576c5bb67ed7324d25e8ba
matetipping <matetipping@gmail.com>
2017-08-05 17:38:49 +1000

    Update and rename tipping_data_109.js to tipping_data_110.js

--- tipping_data_110.js
+++ tipping_data_110.js
@@ -275,7 +275,7 @@ var tipping_data = {
         ["R19", "Shaydog", "HAW", 12, "MEL", 20, "GWS", 24, "PTA", 28, "RIC", 16, "GEE", 14, "ESS", 8, "COL", 27, "WCE", 19, "Patrick Dangerfield (GEE)", 23, "Josh Kennedy (WCE)", 40, "", ""],
         ["R20", "Dannelboyz", "GEE", 2, "GWS", 8, "ESS", 31, "WBD", 46, "COL", 20, "GCS", 1, "STK", 5, "RIC", 5, "ADE", 22, "", "", "Joe Daniher (ESS)", 20, "", ""],
         ["R20", "Harry Rowland", "GEE", 50, "GWS", 25, "ESS", 47, "WBD", 46, "COL", 23, "FRE", 20, "STK", 15, "HAW", 15, "ADE", 20, "", "", "Tom Rockliff (BRI)", "", "", ""],
-        ["R20", "Bigbadbrucey", "GEE", 3, "GWS", 15, "ESS", 33, "WBD", 20, "COL", 11, "FRE", 10, "STK", 20, "RIC", 15, "ADE", 25, "", "", "Joe Daniher (ESS)", "", "", ""],
+        ["R20", "Bigbadbrucey", "GEE", 3, "GWS", 15, "ESS", 33, "WBD", 20, "COL", 11, "FRE", 10, "STK", 20, "RIC", 15, "ADE", 25, "", "", "Joe Daniher (ESS)", 20, "", ""],
         ["R20", "Mann", "SYD", 1, "GWS", 22, "ESS", 30, "WBD", 24, "NTH", 3, "FRE", 5, "STK", 4, "HAW", 8, "ADE", 9, "Tom Mitchell", "", "Josh Kennedy (WCE)", "", "", ""],
         ["R20", "Flagpies", "GEE", 7, "GWS", 14, "ESS", 24, "WBD", 30, "COL", 9, "FRE", 10, "WCE", 15, "HAW", 10, "ADE", 20, "Dustin Martin (RIC)", "", "Taylor Walker (ADE)", "", "", ""],
         ["R20", "Jono22", "GEE", 15, "GWS", 12, "CAR", 5, "WBD", 11, "COL", 13, "FRE", 14, "STK", 18, "HAW", 22, "ADE", 17, "", "", "", "", "", ""],
