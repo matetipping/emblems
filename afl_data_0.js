8ea3e345c2089229bb2cd90fd676a5dc68652e63
matetipping <matetipping@gmail.com>
2018-02-14 21:56:27 +1100

    Update and rename afl_data_19.js to afl_data_0.js

---
+++ afl_data_0.js
@@ -0,0 +1,38 @@
+var afl_data = {
+    repeater: null,
+    dates: [
+        ["R1", "March 22, 2018 19:25:00 GMT+1100"],
+//        ["R2", "March 30, 2017 19:20:00 GMT+1100"],
+//        ["R3", "April 7, 2017 19:50:00 GMT+1000"],
+//        ["R4", "April 13, 2017 18:10:00 GMT+0800"],
+//        ["R5", "April 21, 2017 19:20:00 GMT+0930"],
+//        ["R6", "April 28, 2017 19:50:00 GMT+1000"],
+//        ["R7", "May 5, 2017 19:50:00 GMT+1000"],
+//        ["R8", "May 12, 2017 18:10:00 GMT+0800"],
+//        ["R9", "May 19, 2017 19:50:00 GMT+1000"],
+//        ["R10", "May 26, 2017 19:50:00 GMT+1000"],
+//        ["B1", "June 2, 2017 19:50:00 GMT+1000"],
+//        ["B2", "June 10, 2017 19:25:00 GMT+1000"],
+//        ["R14", "June 22, 2017 19:20:00 GMT+0930"],
+//        ["R15", "June 30, 2017 19:50:00 GMT+1000"],
+//        ["R16", "July 7, 2017 19:20:00 GMT+0930"],
+//        ["R17", "July 14, 2017 19:50:00 GMT+1000"],
+//        ["R18", "July 21, 2017 19:20:00 GMT+930"],
+//        ["R19", "July 28, 2017 19:50:00 GMT+1000"],
+//        ["F1", "August 4, 2017 19:50:00 GMT+1000"],
+//        ["F2", "August 11, 2017 19:50:00 GMT+1000"],
+//        ["F3", "August 18, 2017 19:20:00 GMT+930"],
+//        ["F4", "August 25, 2017 19:50:00 GMT+1000"],
+    ],
+    fixtures: [
+        ["R1", "RIC", "CAR", "MCG - Thursday, March 22 - 7:25 PM"],
+        ["R1", "ESS", "ADE", "ES - Friday, March 23 - 7:50 PM"],
+        ["R1", "STK", "BRI", "ES - Saturday, March 24 - 3:35 PM"],
+        ["R1", "PTA", "FRE", "AO - Saturday, March 24 - 4:05 PM"],
+        ["R1", "GCS", "NTH", "CS - Saturday, March 24 - 6:25 PM"],
+        ["R1", "HAW", "COL", "MCG - Saturday, March 24 - 7:25 PM"],
+        ["R1", "GWS", "WBD", "UNSW - Sunday, March 25 - 1:10 PM"],
+        ["R1", "MEL", "GEE", "MCG - Sunday, March 25 - 3:20 PM"],
+        ["R1", "WCE", "SYD", "OS - Sunday, March 25 - 4:20 PM"]
+    ]
+}
