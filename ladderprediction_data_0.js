53fd0427fe6d51c2055e3a539c58b64cc085859c
matetipping <matetipping@gmail.com>
2018-03-04 23:24:45 +1100

    Update ladderprediction_data_0.js

--- ladderprediction_data_0.js
+++ ladderprediction_data_0.js
@@ -2,6 +2,7 @@ var ladder_data = {
     tips: [
         ["Administrator", "ADE", "GEE", "RIC", "GWS", "PTA", "SYD", "ESS", "WCE", "MEL", "WBD", "STK", "HAW", "COL", "FRE", "NTH", "CAR", "GCS", "BRI"],
         ["Mann", "ADE", "SYD", "PTA", "GEE", "GWS", "ESS", "MEL", "HAW", "RIC", "WBD", "STK", "WCE", "COL", "FRE", "CAR", "NTH", "BRI", "GCS"],
-        ["SCOTT", "GWS", "ADE", "GEE", "SYD", "WBD", "HAW", "GCS", "WCE", "RIC", "COL", "ESS", "CAR", "MEL", "NTH", "PTA", "FRE", "STK", "BRI"]
+        ["SCOTT", "GWS", "ADE", "GEE", "SYD", "WBD", "HAW", "GCS", "WCE", "RIC", "COL", "ESS", "CAR", "MEL", "NTH", "PTA", "FRE", "STK", "BRI"],
+        ["ciniboi_12", "GEE", "ADE", "GWS", "PTA", "SYD", "RIC", "ESS", "STK", "MEL", "HAW", "WBD", "COL", "FRE", "WCE", "BRI", "CAR", "GCS", "NTH"]
     ]
 }
