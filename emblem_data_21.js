8a729314ca30c86885c4c5683b8212806fefc72c
matetipping <matetipping@gmail.com>
2018-03-18 16:41:50 +1100

    Update and rename emblem_data_20.js to emblem_data_21.js

--- emblem_data_21.js
+++ emblem_data_21.js
@@ -10,14 +10,14 @@ var emblem_data = {
         ["Mann", "rgb(148, 8, 8)", "http://b3.ifrm.com/30609/91/0/p3003820/emblemdesigns_blue.png", "-750px 0px", "0", "http://b3.ifrm.com/30609/91/0/p3003812/emblemdesigns_gold.png", "-450px -300px", "0"],
         ["Shaydog", "rgb(13, 13, 13)", "http://b3.ifrm.com/30609/91/0/p3003806/emblemdesigns_maroon.png", "-450px -150px", "270", "http://b3.ifrm.com/30609/91/0/p3003751/emblemdesigns_black.png", "-600px -150px", "180"],
         ["Alex Rowland", "rgb(13, 13, 13)", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-450px -150px", "0", "http://b3.ifrm.com/30609/91/0/p3003763/emblemdesigns_red.png", "-450px -450px", "0"],
-        ["Steve", "rgb(13, 13, 13)", "http://b3.ifrm.com/30609/91/0/p3003812/emblemdesigns_gold.png", "-450px -150px", "90", "http://b3.ifrm.com/30609/91/0/p3003763/emblemdesigns_red.png", "-450px -150px", "180"],
         ["Harry Rowland", "rgb(255, 238, 17)", "http://b3.ifrm.com/30609/91/0/p3005729/emblemdesigns_nutella.png", "-450px -150px", "90", "http://b3.ifrm.com/30609/91/0/p3003804/emblemdesigns_amber.png", "-750px -450px", "0"],
         ["Jeanille01", "rgb(16, 37, 63)", "http://b3.ifrm.com/30609/91/0/p3003813/emblemdesigns_green.png", "0px 0px", "0", "http://b3.ifrm.com/30609/91/0/p3005772/emblemdesigns_royalblue.png", "-300px 0px", "0"],
         ["jamieling", "rgb(250, 198, 196)", "http://b3.ifrm.com/30609/91/0/p3003804/emblemdesigns_amber.png", "-450px -450px", "180", "none", "-600px -300px", "90"],
         ["SCOTT", "rgb(23, 55, 94)", "http://b3.ifrm.com/30609/91/0/p3003816/emblemdesigns_teal.png", "0px -300px", "0", "http://b3.ifrm.com/30609/91/0/p3003817/emblemdesigns_yellow.png", "0px -300px", "0"],
-        ["Kerrynterra", "rgb(56, 249, 109)", "http://b3.ifrm.com/30609/91/0/p3003830/emblemdesigns_violet.png", "-150px -300px", "0", "http://b3.ifrm.com/30609/91/0/p3003828/emblemdesigns_pink.png", "-450px -300px", "0"],
         ["Bill Rowland", "rgb(242, 66, 66)", "http://b3.ifrm.com/30609/91/0/p3003824/emblemdesigns_navy.png", "-150px 0px", "0", "http://b3.ifrm.com/30609/91/0/p3003817/emblemdesigns_yellow.png", "-450px -150px", "0"],
-        ["Kaytley🐐", "rgb(128, 28, 90)", "http://b3.ifrm.com/30609/91/0/p3003804/emblemdesigns_amber.png", "-750px -450px", "0", "http://b3.ifrm.com/30609/91/0/p3003812/emblemdesigns_gold.png", "-450px -300px", "0"]
+        ["Kaytley🐐", "rgb(242, 242, 242)", "http://b3.ifrm.com/30609/91/0/p3005719/emblemdesigns_splurtle.png", "-750px -450px", "0", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-750px -450px", "0"],
+        ["Steve", "rgb(13, 13, 13)", "http://b3.ifrm.com/30609/91/0/p3003807/emblemdesigns_orange.png", "0px -450px", "270", "http://b3.ifrm.com/30609/91/0/p3005724/emblemdesigns_blood.png", "-450px -150px", "0"],
+        ["Kerrynterra", "rgb(31, 188, 0)", "http://b3.ifrm.com/30609/91/0/p3005721/emblemdesigns_midnight.png", "-600px 0px", "0", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-450px -450px", "0"]
     ]
 }
 
