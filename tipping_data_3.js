58138f88eb8631b024892b92ab383a5726b3152e
matetipping <matetipping@gmail.com>
2018-03-18 16:46:07 +1100

    Update and rename tipping_data_2.js to tipping_data_3.js

---
+++ tipping_data_3.js
@@ -0,0 +1,8 @@
+var tipping_data = {
+    round: "R1",
+    admin: "Administrator",
+    tips: [],
+    fixtures: [
+        ["R1", "Craig Terrington", "Kaytley🐐", "Dannelboyz", "ciniboi_12", "jamieling", "Bigbadbrucey", "Mann", "Jono22", "Alex Rowland", "Jeanille01", "Harry Rowland", "Steve", "Shaydog", "Flagpies", "SCOTT", "KerrynTerra", "Bill Rowland"]
+    ]
+}
