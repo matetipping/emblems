21502d0e0edea5ee6fcee2c991e4313eb4fbf8cd
matetipping <matetipping@gmail.com>
2018-03-18 17:14:43 +1100

    Update and rename ladderprediction_data_4.js to ladderprediction_data_5.js

---
+++ ladderprediction_data_5.js
@@ -0,0 +1,13 @@
+var ladder_data = {
+    tips: [
+        ["Alex Rowland", "GWS", "ADE", "GEE", "SYD", "RIC", "PTA", "MEL", "ESS", "STK", "HAW", "WBD", "WCE", "FRE", "CAR", "COL", "NTH", "BRI", "GCS"],
+        ["Kerrynterra", "RIC", "GEE", "SYD", "WBD", "MEL", "HAW", "ADE", "FRE", "WCE", "ESS", "COL", "NTH", "STK", "BRI", "GWS", "PTA", "GCS", "CAR"],
+        ["Jono22", "GEE", "RIC", "ADE", "GWS", "SYD", "ESS", "PTA", "MEL", "STK", "WBD", "HAW", "FRE", "COL", "CAR", "GCS", "BRI", "NTH", "WCE"],
+        ["Bill Rowland", "GWS", "RIC", "ADE", "PTA", "SYD", "GEE", "HAW", "MEL", "ESS", "FRE", "WBD", "STK", "COL", "CAR", "GCS", "WCE", "BRI", "NTH"],
+        ["Harry Rowland", "ADE", "RIC", "GEE", "GWS", "HAW", "PTA", "WBD", "STK", "ESS", "WCE", "SYD", "FRE", "COL", "GCS", "CAR", "MEL", "BRI", "NTH"],
+        ["Kaytley🐐", "HAW", "RIC", "WBD", "SYD", "GEE", "PTA", "WCE", "STK", "FRE", "COL", "MEL", "ESS", "NTH", "ADE", "BRI", "GWS", "CAR", "GCS"],
+        ["Shaydog", "GEE", "HAW", "WBD", "SYD", "ESS", "ADE", "COL", "PTA", "GWS", "NTH", "FRE", "GCS", "CAR", "BRI", "STK", "WCE", "MEL", "RIC"],
+        ["Jeanille01", "GWS", "WBD", "SYD", "HAW", "ADE", "WCE", "STK", "PTA", "GEE", "MEL", "CAR", "FRE", "COL", "ESS", "GCS", "NTH", "RIC", "BRI"],
+        ["jamieling", "GEE", "GWS", "SYD", "WBD", "ADE", "STK", "HAW", "WCE", "RIC", "NTH", "ESS", "FRE", "GCS", "PTA", "COL", "MEL", "CAR", "BRI"]
+    ]
+}
