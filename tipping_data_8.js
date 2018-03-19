a5d719156a28d85bf2c95ca00374ca537490c2e0
matetipping <matetipping@gmail.com>
2018-03-19 15:10:01 +1100

    Update and rename tipping_data_7.js to tipping_data_8.js

---
+++ tipping_data_8.js
@@ -0,0 +1,14 @@
+var tipping_data = {
+    round: "R1",
+    admin: "Administrator",
+    tips: [
+        ["R1", "Bigbadbrucey", "RIC", 37, "ESS", 7, "STK", 35, "PTA", 34, "GCS", 15, "HAW", 7, "GWS", 27, "GEE", 13, "WCE", 5, "", "", "", "", "", ""],
+        ["R1", "Kaytley🐐", "RIC", 10, "ADE", 15, "STK", 5, "PTA", 10, "NTH", 5, "HAW", 20, "WBD", 10, "GEE", 12, "WCE", 10, "", "", "", "", "", ""],
+        ["R1", "Dannelboyz", "RIC", 55, "ADE", 22, "STK", 31, "PTA", 28, "NTH", 8, "HAW", 14, "GWS", 23, "GEE", 17, "SYD", 42, "", "", "Josh Kennedy (WCE)", "", "", ""],
+        ["R1", "Susan", "RIC", 54, "ADE", 24, "STK", 45, "PTA", 51, "GCS", 6, "HAW", 9, "GWS", 30, "GEE", 24, "SYD", 42, "", "", "", "", "", ""]
+    ],
+    fixtures: [
+        ["R1", "Craig Terrington", "Kaytley🐐", "Dannelboyz", "ciniboi_12", "Bigbadbrucey", "Mann", "Jono22", "Alex Rowland", "Jeanille01", "Harry Rowland", "Steve", "Flagpies", "SCOTT", "Kerrynterra", "Bill Rowland", "Susan"],
+        
+    ]
+}
