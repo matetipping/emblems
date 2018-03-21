304b2025e8179dc286f532b586d9f82f72a8b7ee
matetipping <matetipping@gmail.com>
2018-03-21 21:09:53 +1100

    Update and rename tipping_data_9.js to tipping_data_10.js

---
+++ tipping_data_10.js
@@ -0,0 +1,23 @@
+var tipping_data = {
+    round: "R1",
+    admin: "Administrator",
+    tips: [
+        ["R1", "Administrator", "", "", "", "", "", ""],
+        ["R1", "Bigbadbrucey", "RIC", 37, "ESS", 7, "STK", 35, "PTA", 34, "GCS", 15, "HAW", 7, "GWS", 27, "GEE", 13, "WCE", 5, "", "", "", "", "", ""],
+        ["R1", "Kaytley🐐", "RIC", 10, "ADE", 15, "STK", 5, "PTA", 10, "NTH", 5, "HAW", 20, "WBD", 10, "GEE", 12, "WCE", 10, "", "", "", "", "", ""],
+        ["R1", "Dannelboyz", "RIC", 55, "ADE", 22, "STK", 31, "PTA", 28, "NTH", 8, "HAW", 14, "GWS", 23, "GEE", 17, "SYD", 42, "", "", "Josh Kennedy (WCE)", "", "", ""],
+        ["R1", "Susan", "RIC", 54, "ADE", 24, "STK", 45, "PTA", 51, "GCS", 6, "HAW", 9, "GWS", 30, "GEE", 24, "SYD", 42, "", "", "", "", "", ""],
+        ["R1", "Bill Rowland", "RIC", 33, "ADE", 25, "STK", 13, "PTA", 21, "GCS", 9, "HAW", 15, "GWS", 31, "MEL", 12, "SYD", 12, "", "", "Eddie Betts (ADE)", "", "", ""],
+        ["R1", "Harry Rowland", "RIC", 35, "ADE", 37, "STK", 23, "PTA", 12, "GCS", 25, "HAW", 20, "GWS", 34, "GEE", 25, "SYD", 18, "", "", "", "", "", ""],
+        ["R1", "Bigbadbrucey", "RIC", 33, "ESS", 5, "STK", 39, "PTA", 34, "GCS", 18, "HAW", 5, "GWS", 15, "GEE", 18, "SYD", 10, "Dustin Martin (RIC)", "", "", "", "", ""],
+        ["R1", "AThomasz", "RIC", 24, "ADE", 4, "STK", 40, "PTA", 4, "GCS", 4, "HAW", 24, "GWS", 14, "GEE", 34, "SYD", 34, "", "", "", "", "", ""],
+        ["R1", "Sliced_Bread", "RIC", 20, "ADE", 20, "STK", 20, "PTA", 30, "NTH", 10, "HAW", 10, "GWS", 30, "MEL", 10, "SYD", 20, "", "", "", "", "", ""],
+        ["R1", "SCOTT", "RIC", 20, "ADE", 15, "STK", 25, "PTA", 30, "GCS", 10, "HAW", 10, "GWS", 15, "GEE", 5, "SYD", 10, "", "", "", "", "", ""],
+        ["R1", "Kerrynterra", "RIC", 14, "ESS", 5, "STK", 17, "FRE", 7, "GCS", 31, "HAW", 17, "WBD", 13, "GEE", 22, "SYD", 16, "", "", "", "", "", ""],
+        ["R1", "olliedub", "RIC", 24, "ADE", 12, "STK", 18, "PTA", 6, "GCS", 6, "HAW", 30, "WBD", 8, "MEL", 12, "SYD", 12, "", "", "", "", "", ""]
+    ],
+    fixtures: [
+        ["R1", "Craig Terrington", "Kaytley🐐", "Dannelboyz", "ciniboi_12", "jamieling", "Bigbadbrucey", "Mann", "Jono22", "Alex Rowland", "Jeanille01", "Harry Rowland", "Steve", "Flagpies", "SCOTT", "Kerrynterra", "Bill Rowland", "Susan", "AThomasz", "Sliced_Bread", "olliedub"],
+        
+    ]
+}
