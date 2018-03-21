c434ce1a391b545ba0e414ad09b52d52b7500729
matetipping <matetipping@gmail.com>
2018-03-21 20:55:27 +1100

    Update and rename ladderprediction_data_23.js to ladderprediction_data_24.js

--- ladderprediction_data_24.js
+++ ladderprediction_data_24.js
@@ -4,7 +4,7 @@ var ladder_data = {
         ["Dannelboyz", "SYD", "ADE", "GWS", "GEE", "ESS", "MEL", "RIC", "STK", "PTA", "WBD", "HAW", "FRE", "COL", "GCS", "CAR", "WCE", "BRI", "NTH"],
         ["Mann", "ADE", "SYD", "PTA", "GEE", "GWS", "ESS", "MEL", "HAW", "RIC", "WBD", "STK", "WCE", "COL", "FRE", "CAR", "NTH", "BRI", "GCS"],
         ["ciniboi_12", "GEE", "ADE", "GWS", "PTA", "SYD", "RIC", "ESS", "STK", "MEL", "HAW", "WBD", "COL", "FRE", "WCE", "BRI", "CAR", "GCS", "NTH"],
-        ["SCOTT", "GWS", "ADE", "GEE", "SYD", "WBD", "HAW", "GCS", "WCE", "RIC", "COL", "ESS", "CAR", "MEL", "NTH", "PTA", "FRE", "STK", "BRI"],
+        ["SCOTT", "GWS", "ADE", "RIC", "MEL", "PTA", "GEE", "SYD", "WBD", "ESS", "STK", "WCE", "HAW", "GCS", "COL", "FRE", "BRI", "NTH", "CAR"],
         ["Bigbadbrucey", "GEE", "GWS", "ADE", "RIC", "ESS", "STK", "SYD", "WBD", "PTA", "MEL", "WCE", "COL", "HAW", "GCS", "FRE", "BRI", "CAR", "NTH"],
         ["Flagpies", "GEE", "GWS", "ADE", "RIC", "ESS", "WCE", "WBD", "MEL", "COL", "SYD", "PTA", "FRE", "HAW", "CAR", "NTH", "BRI", "GCS", "STK"],
         ["Alex Rowland", "GWS", "ADE", "GEE", "SYD", "RIC", "PTA", "MEL", "ESS", "STK", "HAW", "WBD", "WCE", "FRE", "CAR", "COL", "NTH", "BRI", "GCS"],
@@ -15,9 +15,11 @@ var ladder_data = {
         ["Kaytley🐐", "HAW", "RIC", "WBD", "SYD", "GEE", "PTA", "WCE", "STK", "FRE", "COL", "MEL", "ESS", "NTH", "ADE", "BRI", "GWS", "CAR", "GCS"],
         ["Craig Terrington", "GWS", "ADE", "PTA", "MEL", "RIC", "STK", "HAW", "SYD", "GEE", "WBD", "WCE", "ESS", "CAR", "COL", "FRE", "GCS", "BRI", "NTH"],
         ["Steve", "ADE", "GWS", "GEE", "SYD", "MEL", "RIC", "STK", "ESS", "HAW", "NTH", "WBD", "COL", "WCE", "GCS", "PTA", "FRE", "CAR", "BRI"],
-        ["Shaydog", "GEE", "HAW", "WBD", "SYD", "ESS", "ADE", "COL", "PTA", "GWS", "NTH", "FRE", "GCS", "CAR", "BRI", "STK", "WCE", "MEL", "RIC"],
         ["Jeanille01", "GWS", "ADE", "RIC", "SYD", "WBD", "GEE", "MEL", "ESS", "STK", "HAW", "PTA", "FRE", "CAR", "COL", "BRI", "GCS", "WCE", "NTH"],
         ["jamieling", "GEE", "GWS", "SYD", "WBD", "ADE", "STK", "HAW", "WCE", "RIC", "NTH", "ESS", "FRE", "GCS", "PTA", "COL", "MEL", "CAR", "BRI"],
-        ["Susan", "SYD", "GWS", "GEE", "ADE", "RIC", "MEL", "PTA", "HAW", "WBD", "ESS", "FRE", "STK", "COL", "NTH", "WCE", "BRI", "GCS", "CAR"]
+        ["Susan", "SYD", "GWS", "GEE", "ADE", "RIC", "MEL", "PTA", "HAW", "WBD", "ESS", "FRE", "STK", "COL", "NTH", "WCE", "BRI", "GCS", "CAR"],
+        ["AThomasz", "FRE", "SYD", "GEE", "GWS", "STK", "RIC", "ADE", "ESS", "WBD", "HAW", "NTH", "WCE", "BRI", "CAR", "PTA", "GCS", "COL", "MEL"],
+        ["Sliced_Bread", "ADE", "GWS", "RIC", "SYD", "PTA", "MEL", "WBD", "ESS", "GEE", "HAW", "COL", "WCE", "STK", "FRE", "BRI", "CAR", "NTH", "GCS"],
+        ["olliedub", "SYD", "RIC", "WBD", "MEL", "GWS", "ADE", "GEE", "ESS", "PTA", "STK", "FRE", "CAR", "WCE", "HAW", "COL", "BRI", "GCS", "NTH"]
     ]
 }
