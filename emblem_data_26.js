8dd4b6c743391c754ea79c1eb1a6e96eb3f50823
matetipping <matetipping@gmail.com>
2018-03-21 20:58:55 +1100

    Update and rename emblem_data_25.js to emblem_data_26.js

--- emblem_data_26.js
+++ emblem_data_26.js
@@ -18,7 +18,10 @@ var emblem_data = {
         ["Steve", "rgb(13, 13, 13)", "http://b3.ifrm.com/30609/91/0/p3003807/emblemdesigns_orange.png", "0px -450px", "270", "http://b3.ifrm.com/30609/91/0/p3005724/emblemdesigns_blood.png", "-450px -150px", "0"],
         ["Kerrynterra", "rgb(31, 188, 0)", "http://b3.ifrm.com/30609/91/0/p3005721/emblemdesigns_midnight.png", "-600px 0px", "0", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-450px -450px", "0"],
         ["Kaytley🐐", "rgb(242, 242, 242)", "http://b3.ifrm.com/30609/91/0/p3005719/emblemdesigns_splurtle.png", "-750px -450px", "0", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-750px -450px", "0"],
-        ["Susan", "rgb(242, 66, 66)", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-450px -150px", "0", "http://b3.ifrm.com/30609/91/0/p3005772/emblemdesigns_royalblue.png", "-300px -300px", "0"]
+        ["Susan", "rgb(242, 66, 66)", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-450px -150px", "0", "http://b3.ifrm.com/30609/91/0/p3005772/emblemdesigns_royalblue.png", "-300px -300px", "0"],
+        ["olliedub", "rgb(57, 63, 96)", "http://b3.ifrm.com/30609/91/0/p3003817/emblemdesigns_yellow.png", "-600px -150px", "0", "http://b3.ifrm.com/30609/91/0/p3003817/emblemdesigns_yellow.png", "-150px -450px", "0"],
+        ["AThomasz", "rgb(60, 48, 82)", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-750px -150px", "0", "http://b3.ifrm.com/30609/91/0/p3003819/emblemdesigns_azure.png", "-300px -450px", "0"],
+        ["Sliced_Bread", "rgb(40, 40, 40)", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-750px -450px", "180", "http://b3.ifrm.com/30609/91/0/p3003755/emblemdesigns_white.png", "-750px -450px", "0"]
     ]
 }
 
