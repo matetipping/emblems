558e9df808ed00983c2c6d87c742301b97c34b2d
matetipping <matetipping@gmail.com>
2018-04-06 10:18:04 +1000

    Update and rename tipping_pro_data_1.js to tipping_pro_data_2.js

--- tipping_pro_data_2.js
+++ tipping_pro_data_2.js
@@ -1,5 +1,5 @@
 tipping_pro_data = {
-    access: ["Administrator", "ciniboi_12", "Mann", "Alex Rowland", "Dannelboyz", "Kaytley🐐", "Bill Rowland", "Harry Rowland"],
+    access: ["Administrator", "ciniboi_12", "Mann", "Alex Rowland", "Dannelboyz", "Kaytley🐐", "Bill Rowland", "Harry Rowland", "Craig Terrington", "Kerrynterra"],
     all_players: ["Alex Rowland", "AThomasz", "Bigbadbrucey", "Bill Rowland", "ciniboi_12", "Craig Terrington", "Dannelboyz", "Flagpies", "Harry Rowland", "jamieling", "Jeanille01", "Jono22", "Kaytley🐐", "Kerrynterra", "Mann", "olliedub", "SCOTT", "Shaydog", "Sliced_Bread", "Steve", "Susan"],
     players: ["Alex Rowland", "AThomasz", "Bigbadbrucey", "Bill Rowland", "ciniboi_12", "Craig Terrington", "Dannelboyz", "Flagpies", "Harry Rowland", "jamieling", "Jeanille01", "Jono22", "Kaytley🐐", "Kerrynterra", "Mann", "olliedub", "SCOTT", "Sliced_Bread", "Steve", "Susan"],
     rounds_past: ["2017R1", "2017R2", "2017R3", "2017R4", "2017R5", "2017R6", "2017R7", "2017R8", "2017R9", "2017R10", "2017B1", "2017B2", "2017R14", "2017R15", "2017R16", "2017R17", "2017R18", "2017R19", "2017R20", "2017R21", "2017R22", "2017R23"],
